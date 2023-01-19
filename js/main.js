1098318cd8ccc986379c0415d02059fd57d40ec9
main <Lucas.neves.ead@gmail.com>
2023-01-19 09:42:39 -0300

    funções dos botões da página da tripulação

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var Corpo = document.getElementById("corpo");
+
+Corpo.style.overflow = 'hidden';
+
 var botaoExplorar = document.getElementById("explorar");
 
 var marte = document.getElementById("destinosParte1");
@@ -44,6 +48,32 @@ var Destinos1 = document.getElementById("botDestinos1");
 var Destinos2 = document.getElementById("botDestinos2");
 var Destinos3 = document.getElementById("botDestinos3");
 
+var pagEngenheiraInicial = document.getElementById("tripulacaoParte1");
+var pagComandante = document.getElementById("tripulacaoParte2");
+var pagPiloto = document.getElementById("tripulacaoParte3")
+var pagEspecialista = document.getElementById("tripulacaoParte4")
+
+var Engenheira = document.getElementById("engenheira1");
+var Engenheira1 = document.getElementById("engenheira2");
+var Engenheira2 = document.getElementById("engenheira3");
+
+var Piloto = document.getElementById("piloto");
+var Piloto1 = document.getElementById("piloto1");
+var Piloto2 = document.getElementById("piloto3");
+
+var Especialista = document.getElementById("especialista");
+var Especialista1 = document.getElementById("especialista1");
+var Especialista2 = document.getElementById("especialista2");
+
+var Comandante = document.getElementById("comandante");
+var Comandante1 = document.getElementById("comandante2");
+var Comandante2 = document.getElementById("comandante3");
+
+var fundoBotaoTripulacao = document.getElementById("fundoBotaoTripulacao");
+
+var Lista = document.getElementById("lista");
+var Lista1 = document.getElementById("lista1");
+
 botaoExplorar.onclick = function() {
     marte.style.display = 'block';
     Home.style.display = 'none';
@@ -79,6 +109,26 @@ pagHome3.onclick = function() {
     lua.style.display = 'none';
     europa.style.display = 'none';
     titan.style.display =' none';
+}
+
+pagHome4.onclick = function() {
+    Home.style.display = 'block';
+    Tripulacao1.style.display = 'none';
+}
+
+pagHome5.onclick = function() {
+    Home.style.display = 'block';
+    Tripulacao2.style.display = 'none';
+}
+
+pagHome6.onclick = function() {
+    Home.style.display = 'block';
+    Tripulacao3.style.display = 'none';
+}
+
+pagHome7.onclick = function() {
+    Home.style.display = 'block';
+    Tripulacao4.style.display = 'none';
 }
 
 pagLua.onclick = function() {
@@ -188,4 +238,90 @@ pagTripulacao4.onclick = function() {
 Destinos.onclick = function() {
     lua.style.display = 'block';
     Tripulacao1.style.display = 'none';
+}
+
+Comandante.onclick = function() {
+    pagComandante.style.display = 'block';
+    pagEngenheiraInicial.style.display = 'none';
+    pagEspecialista.style.display = 'none';
+    pagPiloto.style.display = 'none';
+}
+
+Comandante1.onclick = function() {
+    pagComandante.style.display = 'block';
+    pagEngenheiraInicial.style.display = 'none';
+    pagEspecialista.style.display = 'none';
+    pagPiloto.style.display = 'none';
+}
+
+Comandante2.onclick = function() {
+    pagComandante.style.display = 'block';
+    pagEngenheiraInicial.style.display = 'none';
+    pagEspecialista.style.display = 'none';
+    pagPiloto.style.display = 'none';
+}
+
+Especialista.onclick = function() {
+    pagEspecialista.style.display = 'block';
+    pagEngenheiraInicial.style.display = 'none';
+
+    Lista1.style.marginRight = '660px';
+}
+
+Especialista1.onclick = function() {
+    pagEspecialista.style.display = 'block';
+    pagComandante.style.display = 'none';
+
+    Lista1.style.marginRight = '660px';
+}
+
+Especialista2.onclick = function() {
+    pagEspecialista.style.display = 'block';
+    pagPiloto.style.display = 'none';
+
+    Lista1.style.marginRight = '660px';
+}
+
+Piloto.onclick = function() {
+    pagPiloto.style.display = 'block';
+    pagEngenheiraInicial.style.display = 'none';
+
+    Lista.style.marginRight = '660px';
+}
+
+Piloto1.onclick = function() {
+    pagPiloto.style.display = 'block';
+    pagComandante.style.display = 'none';
+
+    Lista.style.marginRight = '660px';
+}
+
+Piloto2.onclick = function() {
+    pagPiloto.style.display = 'block';
+    pagEspecialista.style.display = 'none';
+
+    Lista.style.marginRight = '660px';
+}
+
+Engenheira.onclick = function() {
+    pagEngenheiraInicial.style.display = 'block';
+    pagEspecialista.style.display = 'none';
+    pagPiloto.style.display = 'none';
+    pagComandante.style.display = 'none';
+}
+
+Engenheira1.onclick = function() {
+    pagEngenheiraInicial.style.display = 'block';
+    pagComandante.style.display = 'none';
+    pagPiloto.style.display = 'none';
+}
+
+Engenheira2.onclick = function() {
+    pagEngenheiraInicial.style.display = 'block';
+    pagPiloto.style.display = 'none';
+}
+
+fundoBotaoTripulacao.onclick = function() {
+    pagEngenheiraInicial.style.display = 'block';
+    pagComandante.style.display = 'none';
 }
